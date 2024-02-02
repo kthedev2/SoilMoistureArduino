a2c4dabf74e822c6ebce364f95020418aa67a949
kthedev <kthedev2@gmail.com>
2024-02-02 17:23:49 +0300

    Initial commit

---
+++ SoilMoistureSketch/SoilMoistureSketch.ino
@@ -0,0 +1,10 @@
+//The board: Arduino Nano ATmega328P (old bootloader)
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

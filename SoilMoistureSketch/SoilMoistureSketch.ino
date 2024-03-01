781557a1e5ad16cee4441d2ff4ca27f93404f6e8
DigitalGrizzlyBear <121454392+DigitalGrizzlyBear@users.noreply.github.com>
2024-03-01 21:03:20 +0300

    Update SoilMoistureSketch.ino

--- SoilMoistureSketch/SoilMoistureSketch.ino
+++ SoilMoistureSketch/SoilMoistureSketch.ino
@@ -1,10 +1,30 @@
 //The board: Arduino Nano ATmega328P (old bootloader)
 
+//Pin Setup 
+const int SoilPin1 = A1;
+const int SoilPin2 = A2;
+
+//Variables
+const int LBMoist; //Stands for Lower-Bound Moisture
+const int UBMoist; //Stands for Upper-Bound Moisture
+bool isDry = false;
+bool isWet = false; 
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int Moist1 = analogRead(SoilPin1);
+  int Moist2 = analogRead(SoilPin2);
+  int AVRGMoist = ((Moist1+Moist2) * 0.5);
+  
+  if (AVRGMoist <= LBMoist){
+    isDry = true;
+  }
+  else if (AVRGMoist >= UBMoist){
+    isWet = true;
+  }
 }
